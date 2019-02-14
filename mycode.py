395ad57aaa1799611203d41367b197a1e8a8a27a
Mohammed Amro <mkhamies@gmail.com>
2019-02-14 08:55:07 +0300

    first commit to mycode.py

---
+++ mycode.py
@@ -0,0 +1,31 @@
+def function1:
+	linecode line 1
+	linecode line 2
+	linecode line 3
+	linecode line 4
+	linecode line 5
+	linecode line 6
+	linecode line 7
+	linecode line 8
+	
+	
+def function2:
+	linecode line 2-1
+	linecode line 2-2
+	linecode line 2-3
+	linecode line 2-4
+	linecode line 2-5
+	linecode line 2-6
+	linecode line 2-7
+	linecode line 2-8
+
+
+def function3:
+	linecode line 3-1
+	linecode line 3-2
+	linecode line 3-3
+	linecode line 3-4
+	linecode line 3-5
+	linecode line 3-6
+	linecode line 3-7
+	linecode line 3-8	
